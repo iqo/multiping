6d3af9269dd87253c0f4fa99cc27a99fa628e7fc
niklas <niklas.brynolf@gmail.com>
2020-11-19 22:22:22 +0100

    added ip

--- src/pyping.py
+++ src/pyping.py
@@ -1,6 +1,8 @@
 import os
 import multiprocessing
-
+def default():
+    print("invalid option")
+    
 def ping(startip, endip):
     IpAddress = str(startip) + str(".") + str(endip)
     print("opening: ", IpAddress, " in cmd")
@@ -8,7 +10,20 @@ def ping(startip, endip):
    
 if __name__ == '__main__':
     #jobs = []
-    startIp = input("Please enter a start ip example 192.168.1: ")
+    #startIp
+    print("Press 1 192.168.1 \npress 2 for 10.46.1 \npress 3 10.47.1 (Norway) \n")
+    option = int(input("Your option : "))
+
+    if option == 1:
+        startIp = "192.168"
+    elif option == 2:
+        startIp = "10.46"
+    elif option == 3:
+        startIp = "10.47"
+    else:
+        print("Incorrect option")
+
+    #startIp = input("Please enter a start ip example 192.168.1: ")
     endIp = input("Please enter a end ip example 31: ")
     numberOfSessions = input("Please enter range of windows to open: ")
     for i in range(int(numberOfSessions)):
