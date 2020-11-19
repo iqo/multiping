dc377dfd19f7c81c60d6f2e93d5634f77bf924b1
niklas <niklas.brynolf@gmail.com>
2020-11-19 19:35:40 +0100

    fixed varible name

--- src/pyping.py
+++ src/pyping.py
@@ -8,10 +8,10 @@ def ping(startip, endip):
    
 if __name__ == '__main__':
     #jobs = []
-    startip = input("Please enter a start ip example 192.168.1: ")
-    endip = input("Please enter a end ip example 31: ")
+    startIp = input("Please enter a start ip example 192.168.1: ")
+    endIp = input("Please enter a end ip example 31: ")
     numberOfSessions = input("Please enter range of windows to open: ")
     for i in range(int(numberOfSessions)):
-        process = multiprocessing.Process(target=ping, args= (startip, int(endip) + int(i)))
+        process = multiprocessing.Process(target=ping, args= (startIp, int(endIp) + int(i)))
         #jobs.append(process)
         process.start()
